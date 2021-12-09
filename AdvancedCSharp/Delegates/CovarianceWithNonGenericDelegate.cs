c50d9d889ce6f40325a91716d33077451b5f3f91
Nethrazim <vlad.branzei@gmail.com>
2021-12-09 20:17:21 +0200

    Covariance

---
+++ AdvancedCSharp/Delegates/CovarianceWithNonGenericDelegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCSharp.Delegates
+{
+    public class CovarianceWithNonGenericDelegate : DelegateSample
+    {
+      
+
+        class Vehicle
+        {
+            public Vehicle CreateVehicle()
+            {
+                Vehicle myVehicle = new Vehicle();
+                Console.WriteLine("Inside Vehicle.CreateVehicle, a vehicle object is created.");
+                return myVehicle;
+            }
+        }
+
+        class Bus : Vehicle {
+            public Bus CreateBus()
+            {
+                Bus myBus = new Bus();
+                Console.WriteLine("Inside Bus.CreateBus, a bus object is created.");
+                return myBus;
+            }
+        }
+        delegate Vehicle VehicleDelegate();
+
+        public override void Run()
+        {
+            VehicleDelegate delegate_example = new VehicleDelegate(new Bus().CreateBus);
+            delegate_example.Invoke();
+        }
+    }
+}
--- AdvancedCSharp/Program.cs
+++ AdvancedCSharp/Program.cs
@@ -10,15 +10,18 @@ namespace AdvancedCSharp
     internal class Program
     {
         FirstDelegateSample1 ex1 = new FirstDelegateSample1();
+        CovarianceWithNonGenericDelegate ex2 = new CovarianceWithNonGenericDelegate();
         static void Main(string[] args)
         {
             new Program().RunSamples();
             Console.ReadKey();
+
         }
 
         public void RunSamples()
         {
-            ex1.Run();    
+            //ex1.Run();
+            ex2.Run();
         }
     }
 }
